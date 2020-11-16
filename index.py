740dd46566d097d805c3a4ad96a5943997a417a1
Miguel Almas <miguelalmas@hotmail.com>
2020-11-16 19:03:01 +0000

    More changes to index.py

--- index.py
+++ index.py
@@ -83,4 +83,4 @@ def display_page(pathname):
 
 if __name__ == '__main__':
 #    app.run_server(host='127.0.0.1', debug=True)
-    app.run_server(host='0.0.0.0',port=8080, debug=True)
+    app.run_server(debug=False)
