2a1b2e7de8b2d27a4bdf1b470967929cbd7501b0
Miguel Almas <miguelalmas@hotmail.com>
2020-11-17 15:13:08 +0000

    Responsive

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import dash_bootstrap_components as dbc
 # https://bootswatch.com/lux/
 external_stylesheets = [dbc.themes.LUX]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,    
+	meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 server = app.server
 app.config.suppress_callback_exceptions = True
