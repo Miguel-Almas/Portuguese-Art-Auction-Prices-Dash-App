0d4bad54d099830d8482ad24345365cb8699681f
Miguel Almas <miguelalmas@hotmail.com>
2020-11-19 14:50:22 +0000

    2

--- apps/artworks.py
+++ apps/artworks.py
@@ -186,7 +186,7 @@ layout  = html.Div(children=[
                     ),
                 width=6,xs=12,sm=12,md=6,lg=6,xl=6,),   
             ]),
-        ]),
+        ],style={'margin-top':20}),
 ])
 
 ######################### Callback #####################################
