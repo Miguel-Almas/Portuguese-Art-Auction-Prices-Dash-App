ae87f8316b9a77243cc20440f94d70f5d9237d42
OUTSYSTEMS\mua <miguel.almas@outsystems.com>
2022-10-18 10:51:30 +0100

    Home - Limit maximum size to 200

--- apps/home.py
+++ apps/home.py
@@ -175,7 +175,7 @@ layout = html.Div([
               dcc.Slider(
                 id='slider-dim-1',
                 min=1,
-                max=500,
+                max=200,
                 step=1,
                 value=1,
               ),
@@ -194,7 +194,7 @@ layout = html.Div([
               dcc.Slider(
                 id='slider-dim-2',
                 min=1,
-                max=500,
+                max=200,
                 step=1,
                 value=1,
               ),
